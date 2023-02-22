367d1f5a3a4fc416414923c69821a8015c384f9f
asumpta <sumgathoni@gmail.com>
2023-02-22 20:09:25 +0300

    Done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function inFocus(){
+console.log("Good!")
+    }
+    function blurred(){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={inFocus} onBlur={blurred} >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function typeIn() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={typeIn} />
+        </div>
     )
 }
 
